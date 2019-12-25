59c76f7c3135ba28c9e92d45aa23419751775906
liangwenjin <h5es6css3@gmail.com>
2019-12-25 18:37:24 +0800

    完成订单功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -37,7 +37,9 @@ import {
   Step,
   CheckboxGroup,
   Checkbox,
-  Upload
+  Upload,
+  Timeline,
+  TimelineItem
 } from 'element-ui'
 
 Vue.use(Button)
@@ -95,6 +97,9 @@ Vue.use(CheckboxGroup)
 Vue.use(Checkbox)
 
 Vue.use(Upload)
+
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
